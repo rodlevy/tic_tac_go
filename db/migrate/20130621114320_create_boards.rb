2009c22711095c93fef93232f5a0723b4de726ed
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 13:39:25 -0500

    Fixed missing columns in board migrations

--- db/migrate/20130621114320_create_boards.rb
+++ db/migrate/20130621114320_create_boards.rb
@@ -1,6 +1,9 @@
 class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
+      t.integer :player_1
+      t.integer :player_2
+      t.integer :game_id
       t.boolean :turn
       t.text :table, array: true
     end
