2709c024c21514bd749c9eeb445c5f879199392b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 11:37:35 -0500

    add board

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,7 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/board'  do
+  erb :board
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-	$("a#login-hook").on('click', function(event){
-		event.preventDefault();
-		var url = $(this).attr('href');
-		console.log(url);
-		$.get(url, function(url_response){
-			console.log(url_response);
-			$('.container').hide();
-			$('#login-hook').closest('div').append(url_response);
-		});
-
-	});
-	
-	$('#nav').on('submit','#login-layout', function(event){
-		event.preventDefault();
-		var data = $(this).serialize();	
-		var url = $(this).attr('action');
-		console.log(data);
-		$.post(url, data, function(body){
-			var new_nav = $(body).find('#nav').html();
-			console.log(new_nav)
-			$('#login-layout').hide();
-			$('#nav').html(new_nav);
-			$('.container').show();
-		});
-	});
-	// $('#create-hook').on('click', function(event){
+	// $("a#login-hook").on('click', function(event){
 	// 	event.preventDefault();
 	// 	var url = $(this).attr('href');
-	// 	$.get(url, function(url_body){
-	// 		var form = $(url_body).find('#regular-create').html();
-	// 		$('#create-hook').closest('div').append(form);
+	// 	console.log(url);
+	// 	$.get(url, function(url_response){
+	// 		console.log(url_response);
+	// 		$('.container').hide();
+	// 		$('#login-hook').closest('div').append(url_response);
+	// 	});
+
+	// });
+	
+	// $('#nav').on('submit','#login-layout', function(event){
+	// 	event.preventDefault();
+	// 	var data = $(this).serialize();	
+	// 	var url = $(this).attr('action');
+	// 	console.log(data);
+	// 	$.post(url, data, function(body){
+	// 		var new_nav = $(body).find('#nav').html();
+	// 		console.log(new_nav)
+	// 		$('#login-layout').hide();
+	// 		$('#nav').html(new_nav);
+	// 		$('.container').show();
 	// 	});
 	// });
+	
+
+	// Get the element, add a click listener...
+document.getElementById("parent-list").addEventListener("click",function(e) {
+	// e.target is the clicked element!
+	// If it was a list item
+	if(e.target && e.target.nodeName == "LI") {
+		// List item found!  Output the ID!
+		console.log("List item ",e.target.id.replace("post-")," was clicked!");
+	}
+});
 });
 
 
 
- // 1. ready
- // 2. bind event
- // 3. identify url and any vars you are going to pass
- // 4. get/post
+
